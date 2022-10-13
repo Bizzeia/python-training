b2ca2c65e162969d8ecdd7fba73bbe7b1f2b503f
Bizzeia <bommino.leonardo@iisgalvanimi.edu.it>
2022-10-13 10:45:36 +0000

    variables.py

---
+++ variables.py
@@ -0,0 +1,9 @@
+x = str(3)          #x è una stringa quindi quando stampa mi dice che è tale
+print((type(x)))    
+x = int(x)          #trasforma x in un intero
+print((type(x)))
+y = int(3)          #y è un intero 
+z = float(3)        #float serve per numeri non interi tipo 3.4
+
+x = 3               #riconosce se è float o intero in automatico
+y = "Bizzeria"       
