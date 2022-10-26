177d6ace3905747bef9eefdf348801eeb4b55d0d
Bizzeia <bommino.leonardo@iisgalvanimi.edu.it>
2022-10-26 10:52:21 +0000

    versione3

---
+++ list.py
@@ -0,0 +1,11 @@
+
+#stampa il numero di elementi all'interno della lista
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+list1 = ["abc", 34, True, 40, "male"]
